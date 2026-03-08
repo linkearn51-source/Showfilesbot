9795f25fcd79481f2fd79cda03c40ec2d701cca9
linkearn51-source <linkearn51@gmail.com>
2026-03-09 01:18:06 +0700

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-
-FORCE_CHANNEL = os.getenv("FORCE_CHANNEL")
-STORAGE_CHANNEL = int(os.getenv("STORAGE_CHANNEL"))
-ADMIN_ID = int(os.environ.get("ADMIN_ID"))
