73917a81e47650bc03acaca8655eabbd3833bc50
linkearn51-source <linkearn51@gmail.com>
2026-03-11 01:05:23 +0700

    Update database.py

--- database.py
+++ database.py
@@ -1,19 +1,9 @@
 import sqlite3
 
-conn = sqlite3.connect("data.db")
+conn = sqlite3.connect("database.db")
 cur = conn.cursor()
 
-cur.execute("""
-CREATE TABLE IF NOT EXISTS users(
-id INTEGER PRIMARY KEY
-)
-""")
-
-cur.execute("""
-CREATE TABLE IF NOT EXISTS links(
-code TEXT,
-file_id INTEGER
-)
-""")
+cur.execute("CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY)")
+cur.execute("CREATE TABLE IF NOT EXISTS links(code TEXT,file_id INTEGER)")
 
 conn.commit()
